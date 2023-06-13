e1e3d603f07f72b50bbc34d001a067407af87207
sveta-1970 <ssarisoydan@gmail.com>
2023-06-13 20:00:39 +0300

    Created fetch for monobank and weather

--- code/index.js
+++ code/index.js
@@ -7,4 +7,53 @@
  5 Обробляйте можливі помилки під час виконання запиту та виводьте відповідне повідомлення про помилку.
 */
 
-import { req } from "./method.js";
+//api monobank
+
+const data = req("https://api.monobank.ua/bank/currency");
+
+async function req(url) {
+  const data = await fetch(url);
+  return await data.json();
+} //в результате получаем Promise
+
+data
+  .then((info) => {
+    info.map((product) => {
+      console.log(product);
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+  });
+
+//api nova poshta
+
+const apiKey = "43bd842411af97c9174700033d27b00e"; //для нової пошти
+const headers = new Headers({
+  "Content-Type": "application/json",
+  Authorization: apiKey,
+});
+
+fetch("https://api.novaposhta.ua/v2.0/json/", { headers })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+
+
+//weather api
+const apiKey_weather = "5e558f0e7abf23b620280db81851fd8d";
+const data_weather = req_weather(
+  `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey_weather}&units=metric`
+);
+async function req_weather(url) {
+  const data = await fetch(url);
+  return await data.json();
+} //в результате получаем Promise
+
+const final_data = data_weather
+  .then((info) => {
+    console.log(info);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
--- code/method.js
+++ code/method.js
@@ -1,4 +1,1 @@
-export async function req(url) {
-  const data = await fetch(url);
-  return await data.json();
-} //в результате получаем Promise
+
